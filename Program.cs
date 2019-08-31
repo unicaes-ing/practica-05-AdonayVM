e3ddae9197f9e1c145ef24d950d388e6833448e6
Adonay Valencia <adonayvm32@gmail.com>
2019-08-31 00:15:58 -0600

    Práctica 5 finalizada

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Práctica5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dias();
+            Console.ReadKey();
+        }
+        static void dias()
+        {
+            DateTime hoy = DateTime.Today;
+            String fecha = "31 Diciembre 2019";
+            DateTime cambio = DateTime.Parse(fecha);
+            TimeSpan dif = cambio - hoy;
+            int dias = dif.Days;
+            Console.WriteLine("Faltan {0:N2} dias", dias.ToString());
+
+
+        }
+    }
+}
